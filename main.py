8ee890d06a95f0616a213b88122a7342ab4291e6
Sadisha Nimsara <42792030+nsadisha@users.noreply.github.com>
2019-10-27 16:10:26 +0530

    Enter your name

--- main.py
+++ main.py
@@ -1,6 +1,13 @@
-
-i=0
-
-while (i<10):
-    print("Hello")
-    i=i+1
+name = input("Enter your name: ")
+name = name.strip()
+letters = len(name)
+firstLetter = name[0]
+lastLetter = name[letters-1]
+print()
+print("------------------------------------------------------------------")
+print("Your name is",name+".")
+print("Your name has",letters,"Charactors.")
+print("Your name is started with","'"+firstLetter+"'"," and ends with","'"+lastLetter+"'.")
+print("Thank You...!")
+print("------------------------------------------------------------------")
+print()
