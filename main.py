ecea09f1ea7599698fec721537c4796381da234b
tchamalsha <62301204+tchamalsha@users.noreply.github.com>
2020-10-09 23:37:06 +0530

    adding frequency operation

--- main.py
+++ main.py
@@ -9,6 +9,22 @@ print("Your name is",name+".")
 print("Your name has",letters,"Charactors.")
 print("Your name is started with","'"+firstLetter+"'"," and ends with","'"+lastLetter+"'.")
 print()
+print("printing the frequency")
+print()
+i=0
+r=1
+n=1
+while i<letters:
+    while n<letters:
+        if name[i]==name[n]:
+            r=r+1
+        n=n+1
+    print(name[i],"=>",r)
+    print()
+    i=i+1
+    r=0
+    n=0
+print()
 print("Thank You...!")
 print("------------------------------------------------------------------")
 print()
